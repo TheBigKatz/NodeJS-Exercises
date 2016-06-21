2b9bf04dde0dcb71adcd3949cb6157ff68776c61
Andrew Katz <andrewkz123@yahoo.com>
2016-06-21 11:49:40 -0400

    first few exercises

---
+++ number2.js
@@ -0,0 +1,5 @@
+var sayHello = 'Hello, ';
+var name = process.argv[2];
+
+console.log(sayHello);
+console.log(name);
---
+++ number3.js
@@ -0,0 +1,6 @@
+var firstNumber = process.argv[2];
+var number = Number(firstNumber);
+var secondNumber = process.argv[3];
+var number2 = Number(secondNumber);
+
+console.log(number + number2);
